7b688b24e4f842b3aa6ca7023547cff894e6f258
kingwilson98 <king.wilson98@gmail.com>
2022-02-16 12:52:48 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,15 @@
-var button = document.getElementByID("clickme")
-	count = 0;
-button.onclick = function() {
-	count += 1;
-	button.innerHTML = "Click me: " + count;
-};
+let click = 0;
+function c1click(){
+	click += 1;
+	document.getElementByID("click").innerHTML = click;
+}
+
+let visible = true;
+
+visible = !visible;
+if(visible) {
+	document.getElementByID("3button").style.opacity = "0.0";
+}
+else {
+	document.getElementByID("3button").style.opacity = "1.0";
+}
