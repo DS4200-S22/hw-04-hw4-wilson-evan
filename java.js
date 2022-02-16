f440664434e557f982d17d5c73f7cd32e2748167
kingwilson98 <king.wilson98@gmail.com>
2022-02-16 13:28:28 -0500

    Update java.js

--- java.js
+++ java.js
@@ -3,16 +3,16 @@ let clicks = 0;
 
 function c1click(){
 	clicks += 1;
-	document.getElementByID("clicks").innerHTML = clicks;
+	document.getElementById("clicks").innerHTML = clicks;
 };
 
 let visible = true;
 function c3click(){
 	visible = !visible;
 	if(visible) {
-		document.getElementByID("3button").style.opacity = "0.0";
+		document.getElementById("3button").style.opacity = "0.0";
 	}
 	else {
-		document.getElementByID("3button").style.opacity = "1.0";
+		document.getElementById("3button").style.opacity = "1.0";
 	}
 };
